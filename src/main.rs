0c7cb253c78166e8a287de7d64f379ffe9f9e300
Vladimir Kravchenko <volandevovan2004@gmail.com>
2022-11-20 16:15:07 +0200

    Add std env

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
+use std::env;
+
 use azalea::{pathfinder::State, plugins, Account, Client, Event};
+use azalea_protocol::ServerAddress;
 use futures::future::join_all;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let mut tasks = Vec::new();
 
-    for i in 0..10 {
-        
-        let account = Account::offline(&format!("bot_{i}"));
+    let host = env::var("HOST").unwrap_or("localhost".to_string());
+    let port: u16 = env::var("PORT").unwrap_or("25565".to_string()).parse()?;
+
+    let count: usize = env::var("COUNT").unwrap_or("10".to_string()).parse()?;
+    let prefix = env::var("PREFIX").unwrap_or("bot_".to_string());
+
+
+    println!("host: {}\nport: {}\ncount: {}\nprefix: {}\n", host, port, count, prefix);
+
+    for i in 0..count {
+        let account = Account::offline(&format!("{}_{}", prefix, i));
 
         let task = azalea::start(azalea::Options {
             account,
-            address: "localhost",
+            address: ServerAddress { host: host.clone(), port },
             plugins: plugins![],
             state: State::default(),
             handle,
@@ -19,7 +30,7 @@ async fn main() -> anyhow::Result<()> {
 
         tasks.push(task);
     }
-    
+
     join_all(tasks).await;
 
     Ok(())
@@ -30,14 +41,14 @@ async fn handle(bot: Client, event: Event, state: State) -> anyhow::Result<()> {
         Event::Login => {
             println!("{} logged in", bot.profile.name);
 
-            bot.send_command_packet("rtp").await?;
+            bot.send_command_packet("register").await?;
         }
 
         Event::Chat(m) => {
             println!("{}: {}", bot.profile.name, m.message().to_ansi(None));
         }
 
-        _ => {},
+        _ => {}
     }
 
     // match event {
