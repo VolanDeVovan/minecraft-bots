ae72f3f7f7b9b2dc56f57759f6f681f8903ed06b
Vladimir Kravchenko <volandevovan2004@gmail.com>
2022-11-22 01:14:24 +0200

    Add channel

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use std::env;
 
-use azalea::{pathfinder::State, plugins, Account, Client, Event};
+use azalea::{plugins, Account, Client, Event};
 use azalea_protocol::ServerAddress;
-use futures::future::join_all;
+
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Message {
+    Connected(String),
+}
+
+#[derive(Clone, Debug)]
+struct State {
+    tx: mpsc::Sender<Message>,
+}
+
+impl State {
+    fn new(tx: mpsc::Sender<Message>) -> State {
+        State { tx }
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -12,43 +29,33 @@ async fn main() -> anyhow::Result<()> {
     let count: usize = env::var("COUNT").unwrap_or("10".to_string()).parse()?;
     let prefix = env::var("PREFIX").unwrap_or("bot_".to_string());
 
-    println!(
-        "host: {}\nport: {}\ncount: {}\nprefix: {}\n",
-        host, port, count, prefix
-    );
+    let (tx, mut rx) = mpsc::channel::<Message>(1);
 
-    let mut tasks = Vec::with_capacity(count);
+    let task = tokio::spawn(async move {
+        let username = format!("{}_{}", prefix, 1);
+        let account = Account::offline(&username);
 
-    for i in 0..count {
-        let host = host.clone();
-        let prefix = prefix.clone();
-
-        let task = tokio::spawn(async move {
-            let username = format!("{}_{}", prefix, i);
-            let account = Account::offline(&username);
-
-            azalea::start(azalea::Options {
-                account,
-                address: ServerAddress {
-                    host: host.clone(),
-                    port,
-                },
-                plugins: plugins![],
-                state: State::default(),
-                handle,
-            })
-            .await
-            .unwrap_or_else(|err| {
-                println!("{}: {}", username, err);
-            });
+        azalea::start(azalea::Options {
+            account,
+            address: ServerAddress {
+                host: host.clone(),
+                port,
+            },
+            plugins: plugins![],
+            state: State::new(tx.clone()),
+            handle,
+        })
+        .await
+        .unwrap_or_else(|err| {
+            println!("{}: {}", username, err);
         });
+    });
 
-        tasks.push(task);
+    while let Some(msg) = rx.recv().await {
+        println!("got = {:?}", msg);
     }
 
-    for task in tasks {
-        task.await?;
-    }
+    task.await?;
 
     Ok(())
 }
@@ -58,7 +65,9 @@ async fn handle(bot: Client, event: Event, state: State) -> anyhow::Result<()> {
         Event::Login => {
             println!("{} logged in", bot.profile.name);
 
-            bot.send_command_packet("register").await?;
+            state.tx.send(Message::Connected(bot.profile.name.to_string())).await?;
+
+            bot.send_chat_packet("hello world").await?;
         }
 
         Event::Chat(m) => {
@@ -67,15 +76,6 @@ async fn handle(bot: Client, event: Event, state: State) -> anyhow::Result<()> {
 
         _ => {}
     }
-
-    // match event {
-    //     Event::Initialize => todo!(),
-    //     Event::Login => todo!(),
-    //     Event::Chat(_) => todo!(),
-    //     Event::Tick => todo!(),
-    //     Event::Packet(_) => todo!(),
-    //     Event::UpdatePlayers(_) => todo!(),
-    // }
 
     Ok(())
 }
