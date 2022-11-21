d6d817095f8cf33bacad9e2ec647c93124b0b7f9
Vladimir Kravchenko <volandevovan2004@gmail.com>
2022-11-21 13:43:25 +0200

    Improve

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,49 @@ use futures::future::join_all;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut tasks = Vec::new();
-
     let host = env::var("HOST").unwrap_or("localhost".to_string());
     let port: u16 = env::var("PORT").unwrap_or("25565".to_string()).parse()?;
 
     let count: usize = env::var("COUNT").unwrap_or("10".to_string()).parse()?;
     let prefix = env::var("PREFIX").unwrap_or("bot_".to_string());
 
+    println!(
+        "host: {}\nport: {}\ncount: {}\nprefix: {}\n",
+        host, port, count, prefix
+    );
 
-    println!("host: {}\nport: {}\ncount: {}\nprefix: {}\n", host, port, count, prefix);
+    let mut tasks = Vec::with_capacity(count);
 
     for i in 0..count {
-        let account = Account::offline(&format!("{}_{}", prefix, i));
+        let host = host.clone();
+        let prefix = prefix.clone();
 
-        let task = azalea::start(azalea::Options {
-            account,
-            address: ServerAddress { host: host.clone(), port },
-            plugins: plugins![],
-            state: State::default(),
-            handle,
+        let task = tokio::spawn(async move {
+            let username = format!("{}_{}", prefix, i);
+            let account = Account::offline(&username);
+
+            azalea::start(azalea::Options {
+                account,
+                address: ServerAddress {
+                    host: host.clone(),
+                    port,
+                },
+                plugins: plugins![],
+                state: State::default(),
+                handle,
+            })
+            .await
+            .unwrap_or_else(|err| {
+                println!("{}: {}", username, err);
+            });
         });
 
         tasks.push(task);
     }
 
-    join_all(tasks).await;
+    for task in tasks {
+        task.await?;
+    }
 
     Ok(())
 }
